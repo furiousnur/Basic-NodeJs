008e91c474a69f16fa04ad0b247d862c91ae62de
furiousnur <furiousnur@gmail.com>
2022-08-11 22:17:27 +0600

    first commit for basic node

---
+++ Lesson 1/index.js
@@ -0,0 +1,5 @@
+const data = require("./student");
+
+console.log(data.getName());
+console.log(data.getAge());
+console.log(data.cgpa);
---
+++ Lesson 1/student.js
@@ -0,0 +1,17 @@
+const getName = () =>{
+    return "Nur Alam";
+}
+
+const getAge = () =>{
+    return "27";
+}
+
+const cgpa = 3.45;
+
+// exports.getName = getName();
+// exports.getAge = getAge();
+// exports.cgpa = cgpa;
+
+module.exports = {
+    getAge, getName, cgpa
+}
